aaa0a505ebb24ec37652165648a2e06cfdd16c1d
shubham <gaikwad7777@gmail.com>
2024-01-07 22:14:39 +0530

    cart-route-fix

--- server/controllers/cart.controller.js
+++ server/controllers/cart.controller.js
@@ -1,7 +1,6 @@
 import asyncHandler from "express-async-handler";
 import { validateMongodbId } from "../utils/validateMongodbId.js";
 import { Cart } from "../models/cart.model.js";
-import { Product } from "../models/product.model.js";
 
 const getCart = asyncHandler(async (req, res) => {
   const { _id } = req.user;
@@ -25,16 +24,11 @@ const createCart = asyncHandler(async (req, res) => {
   const { productId, quantity } = req.body;
   try {
     let cart = await Cart.findOne({ orderBy: _id });
-
     if (!cart) {
       cart = await Cart.create({
         orderBy: _id,
         items: [{ product: productId, quantity }],
       });
-      const product = await Product.findById(productId);
-      cart.cartTotal = product.price * quantity;
-      await cart.save();
-      res.json(cart);
     } else {
       const existingItem = cart.items.find((item) =>
         item.product.equals(productId)
@@ -45,22 +39,16 @@ const createCart = asyncHandler(async (req, res) => {
       } else {
         cart.items.push({ product: productId, quantity });
       }
-
-      const itemPromises = cart.items.map(async (item) => {
-        const product = await Product.findById(item.product);
-        const productPrice = product ? product.price : 0;
-        return productPrice * item.quantity;
-      });
-
-      const itemTotals = await Promise.all(itemPromises);
-
-      cart.cartTotal = itemTotals.reduce(
-        (total, itemTotal) => total + itemTotal,
-        0
-      );
-      await cart.save();
-      res.json(cart);
     }
+    await cart.save();
+    cart = await Cart.findOne({ orderBy: _id }).populate("items.product");
+
+    cart.cartTotal = cart.items.reduce(
+      (total, item) => total + item.product.price * item.quantity,
+      0
+    );
+    await cart.save();
+    res.json(cart);
   } catch (error) {
     throw new Error(error);
   }
